465f1fc9a3a0e579e311764687b78d34941c4d3a
Wooseok Daniel Shin <swsda95@naver.com>
2025-11-20 08:45:54 +0900

    Create __init__.py

---
+++ envs/__init__.py
@@ -0,0 +1,7 @@
+"""
+envs package
+
+Contains simulation and real-environment interfaces for TFL-CORAN.
+- o_ran_sim : Lightweight Python simulator for reproducible experiments.
+- (Optional) o_ran_real : Drivers for UERANSIM / Open5GS / QuaDRiGa.
+"""
