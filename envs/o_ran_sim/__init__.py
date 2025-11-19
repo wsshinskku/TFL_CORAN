0f7b7cbcb705e8f7d7bf5382471755b4268af706
Wooseok Daniel Shin <swsda95@naver.com>
2025-11-20 08:46:11 +0900

    Create __init__.py

---
+++ envs/o_ran_sim/__init__.py
@@ -0,0 +1,11 @@
+"""
+o_ran_sim: Lightweight Open RAN simulator
+
+Provides:
+- ORanSimEnv (slot-level UE-gNB simulator)
+- Channel model
+- Traffic generator
+- Mobility model
+- Scheduler logic
+"""
+from .base_env import ORanSimEnv
